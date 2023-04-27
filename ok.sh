6d67022a6dacd04045f74799b3f2e9950fc22d10
Kazuki H <kazukih0205@gmail.com>
2023-04-27 16:30:49 +0900

    Revert "mm/madvise: introduce process_madvise() syscall: an external memory hinting API"
    This reverts commit f4ed73112f319caacab1e82754661401ce9efbbe.
    
    Signed-off-by: Kazuki H <kazukih0205@gmail.com>

---
+++ ok.sh
@@ -0,0 +1,18 @@
+#!bin/bash
+
+nano arch/arm64/include/asm/unistd.h
+nano arch/arm64/include/asm/unistd32.h
+nano arch/ia64/kernel/syscalls/syscall.tbl
+nano arch/m68k/kernel/syscalls/syscall.tbl
+nano arch/microblaze/kernel/syscalls/syscall.tbl
+nano arch/mips/kernel/syscalls/syscall_n32.tbl
+nano arch/mips/kernel/syscalls/syscall_n64.tbl
+nano arch/parisc/kernel/syscalls/syscall.tbl
+nano arch/powerpc/kernel/syscalls/syscall.tbl
+nano arch/s390/kernel/syscalls/syscall.tbl
+nano arch/sh/kernel/syscalls/syscall.tbl
+nano arch/sparc/kernel/syscalls/syscall.tbl
+nano arch/x86/entry/syscalls/syscall_32.tbl
+nano arch/x86/entry/syscalls/syscall_64.tbl
+nano arch/xtensa/kernel/syscalls/syscall.tbl
+nano include/uapi/asm-generic/unistd.h
