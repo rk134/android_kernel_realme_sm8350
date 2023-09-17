3a0d507791f2a0c1507eb42f8b30beea6121db94
Dark-Matter7232 <me@const.eu.org>
2023-09-17 21:59:10 +0530

    binder: drop freezer hacks
    - reverts https://github.com/Dark-Matter7232/android_kernel_realme_sm8350/commit/1a07eadd029e37225f5d350ea8af68ceca11e1de
    
    Change-Id: I2b89e1bcd73bf5aff4cc2d8cc8f37953d9059be5
    Signed-off-by: Dark-Matter7232 <me@const.eu.org>

--- drivers/android/binder.c
+++ drivers/android/binder.c
@@ -5551,7 +5551,6 @@ static long binder_ioctl(struct file *filp, unsigned int cmd, unsigned long arg)
 		}
 		break;
 	}
-#if 0
 	case BINDER_FREEZE: {
 		struct binder_freeze_info info;
 		struct binder_proc **target_procs = NULL, *target_proc;
@@ -5630,7 +5629,6 @@ static long binder_ioctl(struct file *filp, unsigned int cmd, unsigned long arg)
 		}
 		break;
 	}
-#endif
 	case BINDER_ENABLE_ONEWAY_SPAM_DETECTION: {
 		uint32_t enable;
 
